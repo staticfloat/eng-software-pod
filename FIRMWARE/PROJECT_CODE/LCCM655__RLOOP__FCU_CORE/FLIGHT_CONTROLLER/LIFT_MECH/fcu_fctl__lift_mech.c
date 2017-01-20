aaa98799487d33240f9c2ea69d928e66218880cd
nazneenk <nazneenk@hotmail.com>
2017-01-20 18:54:53 -0500

    update lift mech module

--- FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/LIFT_MECH/fcu_fctl__lift_mech.c
+++ FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/LIFT_MECH/fcu_fctl__lift_mech.c
@@ -25,6 +25,13 @@
 
 extern struct _strFCU sFCU;
 
+// add to fcu_core_enums.h in lift mech section:
+typedef enum
+{
+	LIFTMECH_DIR_DOWN = 0U,
+	LIFTMECH_DIR_UP = 1U
+
+}E_FCU__LIFTMECH_DIRECTION;
 
 void vFCU_FLIGHTCTL_LIFTMECH__Release(void)
 {
@@ -33,10 +40,40 @@ void vFCU_FLIGHTCTL_LIFTMECH__Release(void)
 		Luint32 u32PodSpeed = vFCU__POD_SPEED();
 		if (sFCU.sOpStates.u8Lifted && (u32PodSpeed < PODSPEED_STANDBY))
 		{
-			vFCU_FLIGHTCTL_LIFTMECH_DIR(1);	//TODO: assuming 1 is up
-			vFCU_FLIGHTCTL_LIFTMECH_SPEED(LIFTMECH_ACTUATOR_NOM_UNLIFT_SPEED);	// TODO: until unlifted state is true
+			vFCU_FLIGHTCTL_LIFTMECH_Dir(LIFTMECH_DIR_UP);	//TODO: assuming 1 is up
+			vFCU_FLIGHTCTL_LIFTMECH_Speed(C_FCU__LIFTMECH_ACTUATOR_NOM_UNLIFT_SPEED);	// TODO: until unlifted state is true
 		}
 	}
+}
+
+void vFCU_FLIGHTCTL_LIFTMECH__SetDirAll(E_FCU__LIFTMECH_DIRECTION dir)
+{
+	Luint8 u8actuatorIndex;
+
+	for (u8actuatorIndex=0; u8actuatorIndex < NUM_LIFTMECH_ACTUATORS; u8actuatorIndex++)
+	{
+		vFCU_FLIGHTCTL_LIFTMECH_Dir(u8actuatorIndex, dir);
+	}
+}
+
+void vFCU_FLIGHTCTL_LIFTMECH__SetSpeedAll(Luint32 u32speed)
+{
+	Luint8 u8actuatorIndex;
+
+	for (u8actuatorIndex=0; u8actuatorIndex < NUM_LIFTMECH_ACTUATORS; u8actuatorIndex++)
+	{
+		vFCU_FLIGHTCTL_LIFTMECH_Speed(u8actuatorIndex, u32speed);
+	}
+}
+
+void vFCU_FLIGHTCTL_LIFTMECH_Dir(Luint8 u8ActuatorIndex, E_FCU__LIFTMECH_DIRECTION dir)
+{
+	// need to interface with lower level stuff here
+}
+
+void vFCU_FLIGHTCTL_LIFTMECH_Speed(Luint8 u8ActuatorIndex, Luint32 u32speed)
+{
+	// need to interface with lower level stuff here
 }
 
 #endif //C_LOCALDEF__LCCM655__ENABLE_LIFT_MECH_CONTROL
