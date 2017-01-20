ba76c41dea46a5596df4d5d1c988752555111bec
nazneenk <nazneenk@hotmail.com>
2017-01-20 09:33:36 -0500

    clean up pod drive module

--- FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/POD_DRIVE/fcu__fctl__pod_drive.c
+++ FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/POD_DRIVE/fcu__fctl__pod_drive.c
@@ -1,7 +1,7 @@
 /**
 * @file       FCU__FCTRL__POD_DRIVE.C
 * @brief      Pod drive control
-* @author
+* @author	  Nazneen Khan
 * @copyright  rLoop Inc.
 */
 /**
@@ -25,43 +25,83 @@
 
 extern struct _strFCU sFCU;
 
-// TODO: need the following constants and variables implemented:
-#define POD_DRIVE_GS_COMM_LOSS_DELAY	10U	//TODO: find this value
-#define POD_DRIVE_PRERUN_MAX_DELAY		10U //TODO: find this value
-#define POD_TARGET_POINT_MARGIN_POS		10U //TODO: find this value
-#define POD_STOP_X_POS					10U //TODO: find this value
-#define PODSPEED_STANDBY				10U //BORROWED FROM HOVER ENGINE CONTROL
-#define PODSPEED_MAX_SPEED_TO_STABILIZE 10U //TODO: find this value
-#define PODSPEED_TOO_HIGH				10U
-//add POD_STOP to E_GS_COMMANDS
-Luint32 gs_comm_loss_timer;					//TODO: find where this is defined
-Luint32 pre_run_state_timer;
+// TODO: need values for following constants and move to fcu_core_defines.h
+#define C_FCU__GS_COMM_LOSS_DELAY	// should go in udp rx section
+#define C_FCU__POD_TARGET_POINT_MARGIN_POS
+#define C_FCU__POD_STOP_X_POS
+#define C_FCU__PODSPEED_STANDBY
+#define C_FCU__PODSPEED_MAX_SPEED_TO_STABILIZE
+#define C_FCU__PODSPEED_TOO_HIGH
+#define C_FCU__LIFTMECH_RETRACTED_MLP_DISTANCE
+#define C_FCU__LIFTMECH_ACTUATOR_NOM_UNLIFT_SPEED
 
-// TODO: need the following functions implemented:
-//vFCU_FLIGHTCTL_HOVERENGINES__disable()
+// TODO: add variable to fcu_core.h inside of sUDPDiag
+// implement like sFCU.sPodDrive.u8100MS_Timer (reset after heartbeat is received?, heartbeat to be implemented)
+// need 10MS ISR func for udp to increment this
+Luint32 u32_10MS_GS_COMM_Timer;
+// add vFCU_NET_RX__GetGsCommTimer() to fcu_core_net_rx.c
+
+// TODO: add struct to fcu_core.h inside of _strFCU:
+#if C_LOCALDEF__LCCM655__ENABLE_PODDRIVE_CONTROL == 1U
+struct
+{
+	E_FCU__PODDRIVE_PRERUN_STATE ePreRunState;
+	E_FCU__PODDRIVE_GS_COMM eGSCommand;
+	Luint8 u8100MS_Timer;
+
+}sPodDrive;
+#endif
+
+// add to fcu_core_enums.h in pod drive section:
+typedef enum
+{
+	PODDRIVE_GS_POD_STOP = 0U
+}E_FCU__PODDRIVE_GS_COMM;
+
+typedef enum
+{
+	PODDRIVE_PRERUN_INITIAL_STATE = 0U,
+	PODDRIVE_PRERUN_START_HE_STATE = 1U,
+	PODDRIVE_PRERUN_RETRACT_LIFTMECH = 2U,
+	PODDRIVE_PRERUN_GIMBAL_BACKWARD = 3U,
+	PODDRIVE_PRERUN_GIMBAL_MAINTAIN = 4U
+}E_FCU__PODDRIVE_PRERUN_STATE;
+
+// add to fcu_core_enums.h in pod drive section:
+typedef enum
+{
+	GIMBAL_NEUTRAL_LEVEL = 0U,
+	GIMBAL_BACKWARD_LEVEL = 1U,
+	GIMBAL_FORWARD_LEVEL = 2U
+}E_FCU__GIMBAL_LEVEL;
+
+
+
+// TODO: need the following functions:
 //vFCU_FLIGHTCTL_HOVERENGINES__stop()
-//vFCU_FLIGHTCTL_HOVERENGINES__enable()
 //vFCU_FLIGHTCTL_HOVERENGINES__start()
-//vFCU_FLIGHTCTL_AUX_PROP__stop()
-//vFCU_FLIGHTCTL_AUX_PROP__disable()
-//vFCU_FLIGHTCTL_GIMBAL__backward_drive_level()
-//vFCU_FLIGHTCTL_GIMBAL__maintain_against_pusher_level()
-//vFCU_FLIGHTCTL_GIMBAL__horizontal_level()
-//vFCU_FLIGHTCTL_GIMBAL__forward_drive_level()
-//vFCU_FLIGHTCTL_EDDY_BRAKES__release()
-//vFCU_FLIGHTCTL_EDDY_BRAKES__disable()
-//vFCU_FLIGHTCTL_XXXXXX__get_front_pod_pos()
+//vFCU_FLIGHTCTL_AUX_PROP__Stop()
+//vFCU_FLIGHTCTL_AUX_PROP__Disable()
+//vFCU_FLIGHTCTL_GIMBAL__SetLevel(GIMBAL_BACKWARD_LEVEL/GIMBAL_NEUTRAL_LEVEL/GIMBAL_FORWARD_LEVEL)
+//vFCU_FLIGHTCTL_EDDY_BRAKES__Release()
+//vFCU_FLIGHTCTL_EDDY_BRAKES__GainScheduleController(speed)	// pid controller for brakes until position < C_FCU__POD_STOP_X_POS
+//vFCU_FLIGHTCTL_EDDY_BRAKES__GimbalSpeedController() // pid controller for gimbals when speed < C_FCU__PODSPEED_MAX_SPEED_TO_STABILIZE
+//vFCU_FLIGHTCTL_EDDY_BRAKES__ApplyFullBrakes()  // add full eddy brakes until speed < C_FCU__PODSPEED_STANDBY
+//vFCU_FLIGHTCTL_XXXXXX__GetFrontPos()
+//vFCU_FLIGHTCTL_LIFTMECH__Dir(index,dir)
+//vFCU_FLIGHTCTL_LIFTMECH__Speed(index, speed)
+//vFCU_FLIGHTCTL_LIFTMECH__Get_MLP()
+//vFCU_PUSHER__GetState()
 //vFCU__POD_SPEED()	// BORROWED FROM HOVER ENGINE CONTROL
-//vFCU_FLIGHTCTL_GET_GS_CMD()
+//vFCU_NET_RX__GetGsCommTimer()
 
 
 
-void vFCU_FLIGHTCTL_PODDRIVE__stop(void)
+void vFCU_FLIGHTCTL_PODDRIVE__Stop(void)
 {
 	vFCU_FLIGHTCTL_HOVERENGINES__stop();
-	vFCU_FLIGHTCTL_HOVERENGINES__disable();
-	vFCU_FLIGHTCTL_AUX_PROP__stop();
-	vFCU_FLIGHTCTL_AUX_PROP__disable();
+	vFCU_FLIGHTCTL_AUX_PROP__Stop();
+	vFCU_FLIGHTCTL_AUX_PROP__Disable();
 }
 
 
@@ -70,123 +110,158 @@ void vFCU_FLIGHTCTL_PODDRIVE__Process(void)
 
 	switch(sFCU.eMissionPhase)
 	{
+		Luint32 u32PodSpeed;
+		Luint32 u32PodPos;
+
 		case MISSION_PHASE__TEST_PHASE:
-			if (gs_comm_loss_timer > POD_DRIVE_GS_COMM_LOSS_DELAY)
+			if (vFCU_NET_RX__GetGsCommTimer() > C_FCU__GS_COMM_LOSS_DELAY)
 			{
-				vFCU_FLIGHTCTL_PODDRIVE__stop();
+				vFCU_FLIGHTCTL_PODDRIVE__Stop();
 			}
 			break;
 
 		case MISSION_PHASE__PRE_RUN_PHASE:
-			if (gs_comm_loss_timer > POD_DRIVE_GS_COMM_LOSS_DELAY)
+			Luint8 u8LiftMechIndex;
+
+			if (vFCU_NET_RX__GetGsCommTimer() > C_FCU__GS_COMM_LOSS_DELAY)
 			{
-				vFCU_FLIGHTCTL_PODDRIVE__stop();
+				vFCU_FLIGHTCTL_PODDRIVE__Stop();
 			}
 			else
 			{
-				vFCU_FLIGHTCTL_EDDY_BRAKES__release();
-				vFCU_FLIGHTCTL_EDDY_BRAKES__disable();
 				switch(sFCU.sPodDrive.ePreRunState)
-				{
-					// TODO: how to handle timeout for this state machine
-					// who initializes this timer?  and do we go to the next state if timeout?
-					// or go back to  init state? In addition to reporting error to GS
-					case PODDRIVE_PRERUN_INIT:
-						if (if(sFCU.sOpStates.u8Lifted && (u32PodSpeed < PODSPEED_STANDBY)))
-						{
-							sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_START_HOVER_ENGINES;
-						}
-						break;
-					case PODDRIVE_PRERUN_START_HOVER_ENGINES:
-						vFCU_FLIGHTCTL_HOVERENGINES__enable();
+				case PODDRIVE_PRERUN_INITIAL_STATE:
+					if (sFCU.sPodDrive.u8100MS_Timer == 0)
+					{
+						vFCU_FLIGHTCTL_EDDY_BRAKES__Release();
+					}
+					u32PodSpeed = vFCU__POD_SPEED();
+					if (sFCU.sOpStates.u8Lifted && (u32PodSpeed < C_FCU__PODSPEED_STANDBY)))
+					{
+						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_START_HE_STATE;
+						sFCU.sPodDrive.u8100MS_Timer = 0;
+					}
+					if (sFCU.sPodDrive.u8100MS_Timer >= 100)
+					{
+						// TODO: report error to ground station
+						// exit?
+					}
+					break;
+				case PODDRIVE_PRERUN_START_HE_STATE:
+					if (sFCU.sPodDrive.u8100MS_Timer == 0)
+					{
 						vFCU_FLIGHTCTL_HOVERENGINES__start();
-						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_PREPARE_FOR_PUSH;
-						break;
-					case PODDRIVE_PRERUN_PREPARE_FOR_PUSH:
-						vFCU_FLIGHTCTL_GIMBAL__backward_drive_level();
-						// not done here yet
-						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_WAIT_FOR_PUSH;
-						break;
-					case PODDRIVE_PRERUN_WAIT_FOR_PUSH:
-						vFCU_FLIGHTCTL_GIMBAL__maintain_against_pusher_level();
-						// not done here yet
-						break;
-				}
-
+					}
+					if (vFCU_FLIGHTCTL_HOVERENGINES__Get_State() == HOVERENGINES_STATE__STATIC_HOVERING)
+					{
+						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_RETRACT_LIFTMECH;
+						sFCU.sPodDrive.u8100MS_Timer = 0;
+					}
+					if (sFCU.sPodDrive.u8100MS_Timer >= 100)
+					{
+						// TODO: report error to ground station
+						// exit?
+					}
+					break;
+				case PODDRIVE_PRERUN_RETRACT_LIFTMECH:
+					if (sFCU.sPodDrive.u8100MS_Timer == 0)
+					{
+						for (u8LiftMechIndex=0; u8LiftMechIndex < NUM_LIFTMECH_ACTUATORS; u8LiftMechIndex++)
+						{
+							vFCU_FLIGHTCTL_LIFTMECH_Dir(u8LiftMechIndex, 1);	//TODO: assuming 1 is up
+							vFCU_FLIGHTCTL_LIFTMECH_Speed(u8LiftMechIndex, C_FCU__LIFTMECH_ACTUATOR_NOM_UNLIFT_SPEED);
+						}
+					}
+					if (vFCU_FLIGHTCTL_LIFTMECH__Get_MLP() < C_FCU__LIFTMECH_RETRACTED_MLP_DISTANCE)
+					{
+						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_GIMBAL_BACKWARD;
+						sFCU.sPodDrive.u8100MS_Timer = 0;
+					}
+					if (sFCU.sPodDrive.u8100MS_Timer >= 100)
+					{
+						// TODO: report error to ground station
+						// exit?
+					}
+					break;
+				case PODDRIVE_PRERUN_GIMBAL_BACKWARD:
+					if (sFCU.sPodDrive.u8100MS_Timer == 0)
+					{
+						vFCU_GIMBAL__SetLevel(GIMBAL_BACKWARD_LEVEL);
+					}
+					if (vFCU_PUSHER__GetState() == 1U)	// connected to pusher
+					{
+						sFCU.sPodDrive.ePreRunState = PODDRIVE_PRERUN_GIMBAL_MAINTAIN;
+						sFCU.sPodDrive.u8100MS_Timer = 0;
+					}
+					if (sFCU.sPodDrive.u8100MS_Timer >= 100)
+					{
+						// TODO: report error to ground station
+						// exit?
+					}
+					break;
+				case PODDRIVE_PRERUN_GIMBAL_MAINTAIN:
+					// maintain gimbal: do nothing, we are already gimbaling backward
+					break;
 			}
 			break;
 
 		case MISSION_PHASE__PUSH_INTERLOCK_PHASE:
-			if (gs_comm_loss_timer > POD_DRIVE_GS_COMM_LOSS_DELAY)
+			if (vFCU_NET_RX__GetGsCommTimer() > C_FCU__GS_COMM_LOSS_DELAY)
 			{
-				vFCU_FLIGHTCTL_PODDRIVE__stop();
+				vFCU_FLIGHTCTL_PODDRIVE__Stop();
 			}
 			else
 			{
-				vFCU_FLIGHTCTL_EDDY_BRAKES__release();
-				vFCU_FLIGHTCTL_EDDY_BRAKES__disable();
-				vFCU_FLIGHTCTL_GIMBAL__horizontal_level();
+				vFCU_FLIGHTCTL_EDDY_BRAKES__Release();
+				vFCU_FLIGHTCTL_GIMBAL__SetLevel(GIMBAL_NEUTRAL_LEVEL);
 			}
 			break;
 
 
 		case MISSION_PHASE__FLIGHT_MODE:
-			Luint32 u32PodPos = vFCU_FLIGHTCTL_XXXXXX__get_front_pod_pos();
-			Luint32 u32PodSpeed = vFCU__POD_SPEED();
-			E_GS_COMMANDS gs_cmd = vFCU_FLIGHTCTL_GET_GS_CMD();
-			if (gs_cmd == POD_STOP) || (u32PodSpeed > PODSPEED_TOO_HIGH) ||
-					(FCU watchdog indicates a failure) || (BMS failure require emergency breaking))
+			u32PodPos = vFCU_FLIGHTCTL_XXXXXX__GetFrontPos();
+			u32PodSpeed = vFCU__POD_SPEED();
+
+			if (sFCU.sPodDrive.eGSCommand == PODDRIVE_GS_POD_STOP;) || (u32PodSpeed > C_FCU__PODSPEED_TOO_HIGH) || (FCU watchdog failure))
 			{
 				// controlled emergency breaking
-				// add full eddy brakes until speed < PODSPEED_STANDBY
+				vFCU_FLIGHTCTL_EDDY_BRAKES__ApplyFullBrakes();  // add full eddy brakes until speed < C_FCU__PODSPEED_STANDBY
 			}
 			else
 			{
-				if ((POD_STOP_X_POS - u32PodPos) > POD_TARGET_POINT_MARGIN_POS)
+				vFCU_FLIGHTCTL_EDDY_BRAKES__GainScheduleController(u32PodSpeed);	// pid controller for brakes until position < C_FCU__POD_STOP_X_POS
+				if (u32PodSpeed >= C_FCU__PODSPEED_MAX_SPEED_TO_STABILIZE)
 				{
-					// add control code:
-					// eddy brakes direction and speed commands for pre-defined speed profile
-				}
-				if (u32PodSpeed > PODSPEED_MAX_SPEED_TO_STABILIZE)
-				{
-					vFCU_FLIGHTCTL_GIMBAL__horizontal_level();
+					vFCU_FLIGHTCTL_GIMBAL__SetLevel(GIMBAL_NEUTRAL_LEVEL);
 				}
 				else
 				{
-					// add control code:
-					// gimballing HE's for pre-defined speed profile
+					vFCU_FLIGHTCTL_EDDY_BRAKES__GimbalSpeedController();	// pid controller for gimbals when speed < C_FCU__PODSPEED_MAX_SPEED_TO_STABILIZE
 				}
 			}
-
-
 			break;
 
-		case RUN_STATE__FLIGHT_ABORT:	//TODO: verify this is post_run_phase
-			if (gs_comm_loss_timer > POD_DRIVE_GS_COMM_LOSS_DELAY)
+		case MISSION_PHASE__POST_RUN:
+			// TODO: need to rethink this part if hover engines are not running in this phase
+			if (vFCU_NET_RX__GetGsCommTimer() > C_FCU__GS_COMM_LOSS_DELAY)
 			{
-				vFCU_FLIGHTCTL_PODDRIVE__stop();
+				vFCU_FLIGHTCTL_PODDRIVE__Stop();
 			}
 			else
 			{
-				Luint32 u32PodPos = vFCU_FLIGHTCTL_XXXXXX__get_front_pod_pos();
+				Luint32 u32PodPos = vFCU_FLIGHTCTL_XXXXXX__GetFrontPos();
 				Luint32 u32PodSpeed = vFCU__POD_SPEED();
-				Luint8 u8Counter;
 
-				if ((POD_STOP_X_POS - u32PodPos) > POD_TARGET_POINT_MARGIN_POS)
+				vFCU_FLIGHTCTL_EDDY_BRAKES__ApplyFullBrakes();  // add full eddy brakes until speed < C_FCU__PODSPEED_STANDBY; keep applying if called again
+				if ((C_FCU__POD_STOP_X_POS - u32PodPos) > C_FCU__POD_TARGET_POINT_MARGIN_POS)
 				{
-					vFCU_FLIGHTCTL_GIMBAL__forward_drive_level();
+					vFCU_FLIGHTCTL_GIMBAL__SetLevel(GIMBAL_FORWARD_LEVEL);	// until reach target position
+					vFCU_FLIGHTCTL_EDDY_BRAKES__Release();
 				}
-				else if (u32PodSpeed < PODSPEED_STANDBY)
+				else if (u32PodSpeed < C_FCU__PODSPEED_STANDBY)
 				{
-					// maybe this can be a function in the hover engine control
-					vFCU_FLIGHTCTL_GIMBAL__horizontal_level();
-					for(u8Counter = 1; u8Counter < 8; u8Counter++)
-					{
-						vFCU_THROTTLE__Set_Throttle(u8Counter, 0, THROTTLE_TYPE__STEP);
-						// Is this needed? vFCU_COOLING__Set_Valve(u8Counter, 0.0, 2.0);
-					}
-					vFCU_FLIGHTCTL_EDDY_BRAKES__release();
-					vFCU_FLIGHTCTL_EDDY_BRAKES__disable();
+					vFCU_FLIGHTCTL_GIMBAL__SetLevel(GIMBAL_NEUTRAL_LEVEL);
+					vFCU_FLIGHTCTL_HOVERENGINES__stop()
 				}
 			}
 			break;
@@ -194,7 +269,19 @@ void vFCU_FLIGHTCTL_PODDRIVE__Process(void)
 }
 
 
-#endif //C_LOCALDEF__LCCM655__ENABLE_HOVERENGINES_CONTROL
+void vFCU_FLIGHTCTL_PODDRIVE__SetPodStopCmd(void)
+{
+	sFCU.sPodDrive.eGSCommand = PODDRIVE_GS_POD_STOP;
+}
+
+void vFCU_FLIGHTCTL_PODDRIVE__100MS_ISR(void)
+{
+	sFCU.sPodDrive.u8100MS_Timer++;
+}
+
+
+
+#endif //C_LOCALDEF__LCCM655__ENABLE_PODDRIVE_CONTROL
 #ifndef C_LOCALDEF__LCCM655__ENABLE_PODDRIVE_CONTROL
 	#error
 #endif
