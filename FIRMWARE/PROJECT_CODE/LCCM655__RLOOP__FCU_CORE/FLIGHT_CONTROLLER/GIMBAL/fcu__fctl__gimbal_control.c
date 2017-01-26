c251f558fa5f6fb4fd9a10360aed7ee5985f1204
Paul <paullehenaff@msn.com>
2017-01-25 19:34:30 -0800

    [update] Done gimbal control

--- FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/GIMBAL/fcu__fctl__gimbal_control.c
+++ FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/GIMBAL/fcu__fctl__gimbal_control.c
@@ -25,50 +25,86 @@
 //the structure
 extern struct _strFCU sFCU;
 
+// TODO add to defines.h
+// angles for each gimballing command
+#define C_FCU__PUSHER_INTERLOCK_STATIC_TILT	0.0
+#define C_FCU__PUSHER_INTERLOCK_HALF_TILT	0.5
+#define C_FCU__PUSHER_INTERLOCK_FULL_TILT 	2.0
+#define C_FCU__NUM_GIMBALS					4U
+
+// TODO add this to the sFCU
 struct
 {
-	E_FCU__GIMBAL_GS_COMM eGSCommand;
-}sGimbalControl;
+	E_FCU__GIMBALS_GS_COMM eGSCommand;
+	Lfloat32 f32GimbalAngle1;
+	Lfloat32 f32GimbalAngle2;
+	Lfloat32 f32GimbalAngle3;
+	Lfloat32 f32GimbalAngle4;
+}sGimbalsControl;
 
-void vFCU_FLIGHTCTL_GIMBAL__Init(void)
+
+// add to fcu_core_enums.h in pod drive section:
+// typedef enum
+// {
+// 	GIMBAL_NEUTRAL_LEVEL = 0U,
+// 	GIMBAL_BACKWARD_LEVEL = 1U,
+// 	GIMBAL_FORWARD_LEVEL = 2U
+// }E_FCU__GIMBAL_LEVEL;
+
+
+/***************************************************************************//**
+ * @brief
+ * Init any gimbal tasks
+ * 
+ */
+void vFCU_FCTL_GIMBALS__Init(void)
 {
-
+	sFCU.sGimbalsControl.eGSCommands = GIMBALS_CTL_DO_NOTHING; // Set the commands from the ground station to DO_NOTHING at startup
 }
 
-//TODO: need the following functions:
-//vFCU_FLIGHTCTL_GIMBAL__SetAngle(DEGREES)
-
-void vFCU_FLIGHTCTL_GIMBAL__Process(void)
+void vFCU_FCTL_GIMBALS__Process(void)
 {
 
-	switch(sFCU.eMissionPhase){
+	vFCU_FCTL_GIMBALS__ManualCommandsHandle();
+
+	switch(sFCU.eMissionPhase)
+	{
 		case MISSION_PHASE__PUSH_INTERLOCK_PHASE:
-
 			// get pod speed	
-			u32PodSpeed = vFCU__POD_SPEED();
-		
-			//get HE rpm
-			u8status = 1U;
-			for(u8Counter = 1U; u8Counter < 8U; u8Counter++)
+			Luint32 u32PodSpeed = vFCU__POD_SPEED();
+			Luint16 u16HoverEngineState = u16FCU_FCTL_HOVERENGINES__Get_State();
+			// if the pod speed is lower than the pod standby speed, the pod is lifted, and the pod is hovering
+			if (u32PodSpeed < PODSPEED_STANDBY && sFCU.sOpStates.u8Lifted != 0U && u16HoverEngineState == E_FCU__HOVERENGINES_STATES_T.HOVERENGINES_STATE__HOVERING)
 			{
-					Luint16 u16Rpm = 0U;
-					s16FCU_ASI__ReadMotorRpm(u8Counter, &pu16Rpm);
-					if(pu16Rpm > (HOVER_ENGINE_CRUISE_RPM - HOVER_ENGINE_RPM_TOLERANCE)) // should be below "nominal rpm". are these the correct vaules?
-						u8status = 0U;
+				Luint8 u8Counter;
+				switch(sFCU.sGimbalsControl.eGSCommands)
+				{
+					case GIMBAL_CTL_M_SET_STATIC:
+						// TODO crate SetAngle function in the low level gimbal driver
+						vFCU__GIMBALS__SetAngle(C_FCU__PUSHER_INTERLOCK_HALF_TILT * -1);
+						break;
+					case GIMBAL_CTL_M_SET_FULL_BACKWARDS:
+						// TODO crate SetAngle function in the low level gimbal driver
+						vFCU__GIMBALS__SetAngle(C_FCU__PUSHER_INTERLOCK_FULL_TILT * -1);
+						break;
+				}
 			}
-
-			// if the pod speed is lower than the pod standby speed, the pod is lifted, and all engines have "nominal rpm"
-			if((u32PodSpeed < PODSPEED_STANDBY) && (sFCU.sOpStates.u8Lifted != 0U) && u16Rpm == 1){
-				//Here we need to check which command was received
-				
-				//set gimbal angle to  0.5° for Static Gimbaling command
-				//set gimbal angle to 2° for Full Backwards Gimbal command and set phase to Ready For Push
-			}
-
+			break;
+		case MISSION_PHASE__FLIGHT_MODE:
+			vFCU__GIMBALS__SetAngle(C_FCU__PUSHER_INTERLOCK_STATIC_TILT);
 			break;
 	}	
 }
 
+void vFCU_FCTL_GIMBALS__ManualCommandsHandle(void)
+{
+	switch(sFCU.sGimbalsControl.eGSCommands)
+	{
+		// TODO add any other commands that may be needed outside a phase
+		default:
+		break;
+	}
+}
 
 
 #endif //C_LOCALDEF__LCCM655__ENABLE_GIMBAL_CONTROL
