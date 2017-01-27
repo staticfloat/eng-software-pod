e5556fb4854051435fcedccc40d17e0067e3b8a8
Paul <paullehenaff@msn.com>
2017-01-26 16:03:54 -0800

    [update] Implemented the cooling commands

--- FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/COOLING/fcu__fctl__cooling.c
+++ FIRMWARE/PROJECT_CODE/LCCM655__RLOOP__FCU_CORE/FLIGHT_CONTROLLER/COOLING/fcu__fctl__cooling.c
@@ -37,110 +37,108 @@ solenoid valve 1 command, ..., solenoid valve 6 command
 //the structure
 extern struct _strFCU sFCU;
 
-// TODO change this to a more scientific valid value?
-#define C_FCU__COOLING__HE_GROUP_STARTUP_DELAY 5U
-#define C_FCU__COOLING__SOLENOID_DUTYCYCLE_ON 500 //ms
-#define C_FCU__COOLING__SOLENOID_DUTYCYCLE_OFF 1500 //ms
-
 struct
 {
-	E_FCU__COOLING_GS_COMM eGSCommand;
+	E_FCU__COOLING_GS_COMM_T eGSCoolingCommand;
 
-		/** The hover engines state machine */
-	E_FCU_COOLING__STATES_T eState;
-
-	/** The hover engines input commands from GS */
-	E_FCU_COOLING__COMMANDS_T u16CoolingCommands;
-
-	Luint8 u8HESolenoid1;
-	Luint8 u8HESolenoid2;
-	Luint8 u8HESolenoid3;
-	Luint8 u8HESolenoid4;
-	Luint8 u8HESolenoid5;
-	Luint8 u8HESolenoid6;
-	Luint8 u8HESolenoid7;
-	Luint8 u8HESolenoid8;
-}sCoolingControl;
+} sCoolingControl;
 
 typedef enum
 {
-	COOLING_STATE__DISABLED,
-	COOLING_STATE__ENABLED
-} E_FCU_COOLING__STATES_T;
-
-typedef enum
-{
-	COOLING_COMMAND__,
-	COOLING_STATE__ENABLED,
-	COOLING_STATE__START_STATIC_HOVERING,
-	COOLING_STATE__STATIC_HOVERING
-
-} E_FCU_COOLING__COMMANDS_T;
+	COOLING_CTL_DO_NOTHING;
+	CLOSE_ALL_VALVES;
+	SET_VALVE;
+	
+} E_FCU__COOLING_GS_COMM_T;
 
 void vFCU_FCTL_COOLING__Init(void)
 {
-	sFCU.sCoolingControl.eGSCommands = GIMBALS_CTL_DO_NOTHING; // Set the commands from the ground station to DO_NOTHING at startup
+	sFCU.sCoolingControl.eGSCommands = COOLING_CTL_DO_NOTHING; // Set the commands from the ground station to DO_NOTHING at startup
 }
-
 
 
 void vFCU_FCTL_COOLING__Process(void)
 {
 	vFCU_FCTL_COOLING__ManualCommandsHandle();
 	
+	Luint32 u32PodSpeed = vFCU__POD_SPEED();
 
-	// switch(sFCU.eMissionPhase)
-	// {
-	// 	case MISSION_PHASE__PUSH_INTERLOCK_PHASE:
-	// 		break;
-	// }
+	switch(sFCU.eMissionPhase)
+	{
+		case MISSION_PHASE__TEST:
+		case MISSION_PHASE__PRE_RUN:
+		case MISSION_PHASE__POST_RUN:
+			/** Main pod command listener*/
+			switch(sFCU.eGSCommands)
+			{
+				case STATIC_HOVERING:
+					if (sFCU.sOpStates.u8Lifted == 1U && u32PodSpeed < PODSPEED_STANDBY)
+					{
+						vFCU_COOLING__Enable();
+					}
+					break;
+				case RELEASE_STATIC_HOVERING: 
+					if (sFCU.sOpStates.u8StaticHovering == 1U && u32PodSpeed < PODSPEED_STANDBY)
+					{
+						vFCU_COOLING__Disable();
+					}
+					break;
+			}
+			break;
+	}
 }
 
 void vFCU_FCTL_COOLING__ManualCommandsHandle(void)
 {
-	Luint8 u8ManualControlActive;
-	Luint32 u32PodSpeed;
-	Luint32 u32GS_RPM = sFCU.sHoverengines.u32HoverenginesRPM_Commands;
-	switch(sFCU.sHoverengines.u16HoverenginesCommands)
+	/** Cooling specific command listener*/
+	switch(sFCU.sCoolingControl.eGSCommands)
 	{
-		case M_SET_SOLENOID1:
-			u32PodSpeed = u32FCU_FLIGHTCTL_NAV__POD_SPEED();
-			if(u32PodSpeed < PODSPEED_STANDBY)
-				vFCU_THROTTLE__Set_Throttle(1U, u32GS_RPM, THROTTLE_TYPE__STEP);
+		// manual commands (for debugging etc)
+		case CLOSE_ALL_VALVES:
+			vPWR_COOLING__Solennoid_TurnAllOff();
+			break;
+		case SET_VALVE:
+			// TODO find/write function to set the state of a valve
 			break;
 	}
-	"Initiate Cooling System Y" from Ground Station (where Y is either 1, ..., 6), the FCU shall continuously set solenoid valve Y command to the duty cycle solenoid valve ON time | solenoid valve OFF time.
-	"Stop Cooling System Y" or "Close solenoid valve Y" from Ground Station (where Y is either 1, ..., 6), the FCU shall set solenoid valve Y command to OFF.
-	"Open solenoid valve Y" from Ground Station (where Y is either 1, ..., 6), the FCU shall set solenoid valve Y command to ON.
-
-	// switch(sFCU.sCoolingControl.eGSCommands)
-	// {
-
-	// 	Luint32 u32PodSpeed = vFCU__POD_SPEED();
-	// 	Luint16 u16HoverEngineState = u16FCU_FCTL_HOVERENGINES__Get_State();
-
-	// 	if ((sFCU.eMissionPhase == test phase ||
-	// 		sFCU.eMissionPhase == pre-run phase ||
-	// 		sFCU.eMissionPhase == post-run phase) &&
-	// 		sFCU.sOpStates.u8Lifted == 1U &&
-	// 		u32PodSpeed < PODSPEED_STANDBY)
-	// 	{
-
-	// 	}
-	// 	// case:
-	// 	// Upon reception of Command Static Hovering from Ground Station, if the mission phase 
-	// 	// is test phase, pre-run phase or post-run phase, if lifted state is true and if the 
-	// 	// speed is below standby speed, the FCU shall do the following actions, first for 
-	// 	// the {HE1, HE2, HE5, HE6} group, then for the {HE3, HE4, HE7, HE8} group, with 
-	// 	// HE group start-up delay (to avoid high current load at start-up):
-	// 	// start the HE cooling system for this group of HE by continuously setting the corresponding solenoid valve commands to the duty cycle solenoid valve ON time | solenoid valve OFF time (0.5s / 1.5s, reference: Thermal)
-	// 	// (actuation of HE is specified in 7. Control Hover Engines)
-	// }
 }
 
-void vFCU_COOLING__Set_Valve(Luint8 valveNumber, Luint8 timeOn, Luint8 timeOff);
+/**
+	Nominal commands used by the system
+*/
+void vFCU_COOLING__Enable(void)
 {
+	vPWR_COOLING__Enable(1U);
+}
 
+void vFCU_COOLING__Disable(void)
+{
+	vPWR_COOLING__Enable(0U);	
+}
+
+/**
+	Manual commands used for debugging
+*/
+void vFCU_COOLING__Valve_Enable(Luint32 u32SolennoidNumber)
+{
+	// TODO find out what channel and pin numbers to use
+	// Possible pin numbers:
+	// 8U
+	// 16U
+	// 22U
+	// 23U
+	vPWR_COOLING__Solennoid_TurnOn(N2HET_CHANNEL__1, u32SolennoidNumber);
+}
+
+void vFCU_COOLING__Valve_Disable(Luint32 u32SolennoidNumber)
+{
+	// TODO find out what channel and pin numbers to use
+	// Possible pin numbers:
+	// 8U
+	// 16U
+	// 22U
+	// 23U
+	vPWR_COOLING__Solennoid_TurnOff(N2HET_CHANNEL__1, u32SolennoidNumber);
 }
 
 
